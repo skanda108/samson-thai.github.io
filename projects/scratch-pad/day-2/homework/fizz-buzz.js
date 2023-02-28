99607ded8ad9e526eda1497111bcb4295c1b3d67
Samson Thai <sbthai94@gmail.com>
2023-02-28 21:38:48 +0000

    finished fizzbuzz challenge

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -14,6 +14,19 @@ function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
 
+    //if else read from top to bottom so do the strictest case first
+    for (var i = 1; i < 101; i++){
+        if (i % 3 === 0 && i % 5 === 0){ //if a number is a multiple of 3 AND 5 write FizzBuzz
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0){        //if a number is a multiple of 3 write Fizz
+            console.log('Fizz');
+        } else if (i % 5 === 0){        //if a number is a multiple of 5 write Buzz
+            console.log('Buzz');
+        } else {                        //all other cases print the number
+            console.log(i);
+        }
+        //console.log(i);
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }
