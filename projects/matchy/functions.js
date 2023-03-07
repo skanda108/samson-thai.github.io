76a8be512420a307f94f78c8e76c92ac20574f59
Samson Thai <sbthai94@gmail.com>
2023-03-07 21:49:09 +0000

    finished matchy pt 2

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,25 +13,60 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//creating a function called search. parameter is an array called animals. string
 
+function search(animals, string){
+    for (let i = 0; i < animals.length; i++){
+        if (animals[i].name === string){
+            return animals[i];
+        }
+    }
+    return null;
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function replace(animals, name, replacement){
+    for (let i = 0; i < animals.length; i++){
+        if (animals[i].name === name){
+            animals[i] = replacement;
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//animals is array, name is string
 
+function remove(animals, name){
+    for (let i = 0; i < animals.length; i++){
+        if (animals[i].name === name){
+            animals.splice(i, 1);
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//animals is array, animal is object
 
+function add(animals, animal){
+    animalExists = '';
+    for (let i = 0; i < animals.length; i++){
+        if (animals[i].name === animal.name){
+            animalExists = animal.name;
+        }
+    }
+    if (animalExists !== animal.name && animal.name.length > 0 && animal.species.length > 0){
+        animals.push(animal);
+    }
+}
 
 /**
  * You did it! You're all done with Matchy!
