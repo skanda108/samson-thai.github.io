6e89435682e194790d4640ad9f2a98061429a20b
Samson Thai <sbthai94@gmail.com>
2023-03-12 20:05:37 +0000

    progress on studies operators

---
+++ Studies/operators.js
@@ -0,0 +1,36 @@
+/**
+ * OPERATORS
+ * 
+ * Operators perform actions on data. The actions they perform depend on what type of operator is used.
+ * 
+ * Types of Operators:
+ * 
+ * Assignment operators
+ * Comparison operators
+ * Arithmetic operators
+ * Logical operators
+ * String operators
+ * Conditional (ternary) operators
+ * Unary operators
+ * 
+ */
+
+//ASSIGNMENT (=) assigns a value to a variable
+
+let x = 10; //x now has a value of ten
+
+//COMPARISON compare 2 values and evaluate to either true or false (boolean)
+
+console.log(1 < 2); //true
+console.log(1 >= 2); //false
+
+//STRICT and LOOSE COMPARISON
+// === is stricly equal to          !== strictly not equal to
+// == is loosely equal to           != loosely not equal to
+
+console.log(2 === 2); //true
+console.log(2 == '2'); //true
+console.log(2 === '2'); //false
+
+console.log(2 !== 2); //false
+console.log(2 != '2'); //false
