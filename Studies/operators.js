01167dcaabb4ddc2aa2b19ede14e48420d72f4d1
Samson Thai <sbthai94@gmail.com>
2023-03-12 20:51:13 +0000

    finished operators studies

--- Studies/operators.js
+++ Studies/operators.js
@@ -34,3 +34,54 @@ console.log(2 === '2'); //false
 
 console.log(2 !== 2); //false
 console.log(2 != '2'); //false
+
+//ARITHMETIC operators perform arithmetic on numbers
+
+console.log(10 + 10); //20  addition
+console.log(10 - 5); //5    subtraction
+console.log(2 * 3); //6     multiplication
+console.log(16 / 2); //8    division
+
+//Remainder operator (%) returns the remainder
+console.log(440 % 3); //2
+
+console.log(3 ** 2); //9    exponentiation
+
+//Increment
+let a = 1;
+console.log(++a); //2 shorthand for a + 1 = a
+
+//LOGICAL operators determine logic between variables and values
+
+//logical AND (&&). Both statements must be true for the entire statement to be true
+
+console.log(10 < 11 && 10 > 9); //resolves to true because 10 is less than 11 AND 10 is greater than 9
+console.log(10 < 11 && 10 < 9); //false
+
+//logical OR (||). One of the statements must be true for the entire statement to be true
+console.log(10 < 11 || 10 < 9); //true because the statement on the left is true
+
+//logical NOT (!) flips the boolean from true to false or false to true
+console.log(!(5 === 5)); //false
+console.log(!(6 === 5)); //true
+
+//STRING operators. Strings can be concatenated with +
+let str1 = 'Hello';
+let str2 = 'there';
+console.log(str1 + ' ' + str2); //Hello there
+
+//CONDITIONAL (ternary) operator takes 3 values and then executes an expression depending if the condition is true or false
+//syntax
+//condition ? ifTrue : ifFalse
+
+let age = 28;
+let drink = age >= 21 ? 'Whiskey' : 'Juice';
+
+console.log(drink); //Whiskey because 28 is greater than or equal to 21
+
+//UNARY operator is an operation with only 1 value
+
+//typeof operator returns a string indicating the type of operand
+let word = 'character';
+
+console.log(typeof word); //string
