04fc4f318d52dc3ac10d8197ec31a1c7176b0d67
Samson Thai <sbthai94@gmail.com>
2023-03-24 20:09:33 +0000

    studies loop

--- Studies/loops.js
+++ Studies/loops.js
@@ -17,9 +17,47 @@
  */
 
 //Examples:
-//for loop
+//for loop, count up example
 
 for (let i = 0; i < 10; i++){
     console.log(i);
 };
 
+//the example above logs 0,1,2,3,4,5...,9 to the console.
+//i begins at 0
+//the block of code at line 23, continues as long as i is less than 10, 
+//and i increments by 1
+//the instructions that are executed are logging i, which represents a number, to the console
+
+//for loop, count down example
+
+for (let i = 10; i > 0; i--){
+    console.log(i);
+};
+
+//this example counts 10,9,8,...,1
+//i begins at 10
+//the code at line 35 continues as long as i is greater than 0
+//and then i decreases by 1 every iteration
+
+//Using the for loop over an array
+
+let array = ['one', 'two', 'three', 'four'];
+
+for (let i = 0; i < array.length - 1; i++){
+    console.log(array[i]);
+}
+
+//this example prints the elements of array to the console
+//the 1st iteration runs console.log(array[0]) which evaluates to console.log('one');
+//this continues until i is equal to array.length - 1. array.length - 1 is 3 in this example
+//array.length - 1 is used because of 0 indexing
+
+//iterating backwards over an array
+
+for (let i = array.length - 1; i >= 0; i--){
+    console.log(array[i]);
+}
+
+//this example begins at the last item in an array and counts backwards
+//the intial value of i is array.length - 1 which represents the last item in an array
