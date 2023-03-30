d73aca09b1ec9a15b3c8f6ccf0a77959f137a2ad
Samson Thai <sbthai94@gmail.com>
2023-03-30 20:55:42 +0000

    routine commit

---
+++ Studies/datatypes.js
@@ -0,0 +1,27 @@
+/**
+ * DATATYPES:
+ * 
+ * Values in JavaScript can be organized by datatypes.
+ * 
+ * Datatypes can broadly be broken down into simple or complex.
+ * 
+ * SIMPLE/PRIMITIVE
+ * Simple datatypes include:
+ * Number: numeral values
+ * String: collection of characters between quotes
+ * Boolean: true or false to handle logic
+ * NaN: numeral type that stands for not a number
+ * Undefined: an undefined value
+ * Null: the absence of value
+ * 
+ * They are simple because they are atomic and immutable. They do not hold, collect or aggregate other values.
+ * Operations on simple values return new simple values. They do not alter the original value.
+ * 
+ * COMPLEX
+ * Object: collections of key, value pairs { key: 'value' }
+ * Arrays: collections of values as a list
+ * Functions: perform actions on data and return them
+ * 
+ * 
+ * 
+ */
