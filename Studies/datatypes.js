d73e3062221fd0e6cb0c0e4eb71cb4c3b9e095cf
Samson Thai <sbthai94@gmail.com>
2023-03-31 02:57:13 +0000

    studies datatype

--- Studies/datatypes.js
+++ Studies/datatypes.js
@@ -28,4 +28,49 @@
  * datatype is stored and not its literal value.
  * Changes to the copy do not affect the original.
  * 
+ * PRIMITIVE vs COMPLEX Types:
+ * Variables have a fixed amount of memory and can hold the actual values of primitive types.
+ * 
+ * Varibles can only hold references to values of complex types.
+ * Complex types can be of any size. Arrays can hold any number of elements.
+ * Objects can hold any number of key/value pairs and functions can hold any number of statements.
+ * Because complex types don't have a fixed size, variables store a reference to complex types.
+ * 
  */
+
+//Examples of simple datatypes
+
+let a = 10;         //number
+let b = 'string';   //string
+let c = true;       //boolean
+let d = Nan;        //not a number
+let e = undefined;  //undefined
+let f = null;       //null
+let g = Number.POSITIVE_INFINITY; //infinity
+
+console.log(1 / g); // => 0
+
+//Examples of complex datatypes
+
+let h = ['a', 1, ['nested', 'array'], false];   //array that holds other values
+let i = {firstName: 'Samson', lastName: 'Thai', age: 28};   //object containing key value pairs
+let add = function(num1, num2){             //function that takes in data and returns data
+    return num1 + num2;
+};
+
+// Copy by Value vs Copy by Reference
+//Simple datatypes are copied by value;
+
+var a1 = 5;
+var b1 = a1;    //the value stored in a1 is copied into b1
+
+b1 = 7;     //changing the value at b1 doesn't change what's stored in a1
+console.log(a1); //=> 5;
+
+//Complex datatypes are copied by reference
+
+var j = { key1: 'hello' };
+var k = j;      //the value stored in k is a reference to the value stored in j. they both point to the same object
+
+k.key1 = 'hi';
+console.log(j.key1);    //=> hi
